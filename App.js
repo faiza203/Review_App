b9e7559d6b3896dada31d4b2fb85053e8b2d6f52
faiza203 <fiazfaiza981@gmail.com>
2021-02-06 09:46:04 -0500

    Update styles

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import Home from "./components/home";
 
 export default function App() {
-  return <View style={styles.container}>Review App</View>;
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- components/home.js
+++ components/home.js
@@ -7,8 +7,6 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+      padding : 24,
   },
 });

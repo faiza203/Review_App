64d2a46f2e8994cebc0142d609dba4bc42643346
faiza203 <fiazfaiza981@gmail.com>
2021-02-06 09:21:14 -0500

    Re generate it

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import GetLocation from './components/location'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
 export default function App() {
-  const location = ()=> {
-    console.log("Your location is Manawala");
-  }
-  return ( 
-    <GetLocation location={location} />
-  );
+  return <View>Review App</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/location.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'react-native';
-
-export default function getLocation(props) {
-    return (
-        <Button title="Get Location" onPress={props.location} />
-    );
-}

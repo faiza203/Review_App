7f2a984c6d80fb925792eba116e5cffa991fc874
faiza203 <fiazfaiza981@gmail.com>
2021-02-06 09:36:18 -0500

    Create Review Details Screen component

---
+++ components/reviewDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function ReviewDetails() {
+  return <View style={styles.container}>Review Details Screen</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

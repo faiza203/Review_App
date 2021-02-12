1ad6378a15a1da23c692e52f43e110375efab282
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-02-11 17:09:03 -0800

    Add Buttons to change location

--- components/home.js
+++ components/home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 import globalStyles from "../styles/global";
-export default function Home() {
+export default function Home({navigation}) {
+
+  const pressHandler = () => {
+   navigation.navigate("ReviewDetails")
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Home Screen</Text>
+      <Button title="Go To Review Details" onPress={pressHandler} />
     </View>
   );
 }
--- components/reviewDetails.js
+++ components/reviewDetails.js
@@ -1,8 +1,15 @@
 import React from "react";
-import {  View } from "react-native";
+import { View , Text , Button } from "react-native";
 import globalStyles from "../styles/global";
 
-export default function ReviewDetails() {
-  return <View style={globalStyles.container}>Review Details Screen</View>;
+export default function ReviewDetails({navigation}) {
+  const pressHandler = () => {
+    navigation.goBack();
+  }
+
+  return <View style={globalStyles.container}>
+    <Text style={globalStyles.title}>Review Screen</Text>
+    <Button title="Go Back" onPress={pressHandler} />
+  </View>;
 }
 

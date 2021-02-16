d7306eeadb3de6f03b4cd6af82ed8bdc3dbbac51
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-02-15 17:33:23 -0800

    Add Card into ReviewDetails

--- components/reviewDetails.js
+++ components/reviewDetails.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 import globalStyles from "../styles/global";
+import Card from "../shared/card";
 
 export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.title}>{navigation.getParam("title")}</Text>
-      <Text style={globalStyles.title}>{navigation.getParam("body")}</Text>
-      <Text style={globalStyles.title}>{navigation.getParam("rating")}</Text>
+      <Card>
+        <Text style={globalStyles.title}>{navigation.getParam("title")}</Text>
+        <Text style={globalStyles.title}>{navigation.getParam("body")}</Text>
+        <Text style={globalStyles.title}>{navigation.getParam("rating")}</Text>
+      </Card>
     </View>
   );
 }
